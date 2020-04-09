bb53c871715deb1490542955cf9c4dc4940a0c71
Jeú Junior <jeucasulo@hotmail.com>
2020-04-09 16:09:48 -0300

    Test Suites 2/2 Tests 9/9

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {uuid,isUuid} = require('uuidv4'); //8.4K(gziped: 3.4k)
 
 // const { uuid } = require("uuidv4");
 
@@ -9,25 +10,85 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+const likes = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/", (request, response) => {  
+  return response.json({msg:"Server Online"});
+});
+
+app.get("/repositories", (request, response) => {  
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  const likes = 0;
+  const rep = {
+    id : uuid(),title,url,techs,likes  
+  }
+  repositories.push(rep);
+  return response.json(rep);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  if(!isUuid(id)){
+    response.status(400).json({"Error":"Id not found"});
+  }
+  const {title,url,techs} = request.body;    
+  const repIndex = repositories.findIndex(rep=>rep.id===request.params.id)
+  if(repIndex < 0){
+    return response.status(400).json({"Error":"Not Found"})
+  }
+  const likes =  repositories[repIndex].likes;
+  rep = {
+    id,title,url,techs,likes
+  }
+  repositories[repIndex] = rep;
+  return response.json(rep);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const {id} = req.params;
+  if(!isUuid(id)){
+    res.status(400).json({"Error":"Id does not exist"});
+  }
+  const repIndex = repositories.findIndex(rep=>rep.id===id)
+  if(repIndex < 0){
+    res.status(400).json({"Msg":"Not found"})
+  }
+  repositories.splice(repIndex,1); 
+  // return res.json({"Msg":"Removed"})
+  return res.status(204).json()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {user_id} = request.body;
+  const date = Date.now();
+  if(!isUuid(id)){
+    return response.status(400).json({"Error":"Rep do not exists"});
+  }
+  const repIndex = repositories.findIndex(rep => rep.id === id)
+  if(repIndex < 0){
+    return response.status(400).json({"Error":"Rep not found"});
+  }
+
+  const like = {
+    user_id,date
+  }
+
+  // if(!Array.isArray(repositories[repIndex].likes)){    
+  //     repositories[repIndex].likes = [];
+  // }
+ 
+  // repositories[repIndex].likes.push(like);
+  likes.push(like);
+  repositories[repIndex].likes++;
+  // console.log(repositories[repIndex].likes);
+  
+  // return response.json({"likes":repositories[repIndex].length})
+  return response.json({"likes":repositories[repIndex].likes})
 });
 
 module.exports = app;
